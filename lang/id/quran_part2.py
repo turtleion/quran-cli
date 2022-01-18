402e5f7304b1ca1be729e5cd57e964576406b9d5
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-01-18 21:08:08 +0700

    Update user data

--- lang/id/quran_part2.py
+++ lang/id/quran_part2.py
@@ -1,11 +1,11 @@
-# Copyright 2021 by gwbcil6
-# Created by gwbcil6
+# Copyright 2021 by turtleion
+# Created by turtleion
 # Contrib: None
 # Module Used: os,sys,time,requests,mutagen,signal
 # Github: https://github.com/gwbcil6
-# This Github Reposity: https://github.com/gwbcil6/quran-cli/
+# This Github Reposity: https://github.com/turtleion/quran-cli/
 # Requirements: libc 2.33 >, python 3.3 >, mpv, pip
-# Lang.Avaible : id, en, fr (In progress), tk (In progress)
+# Lang.Avaible : id, en
 # Lang.Current : id
 # Run this script as root may can cause error (permissions)
 # 
